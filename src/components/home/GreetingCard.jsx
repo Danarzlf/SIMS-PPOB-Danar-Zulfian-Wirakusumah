9da5c4307b9da19585c104215d0c28f27b058f14
Danarzlf <danarzlf10@gmail.com>
2025-02-16 22:33:39 +0700

    fix

--- src/components/home/GreetingCard.jsx
+++ src/components/home/GreetingCard.jsx
@@ -1,3 +1,4 @@
+// Import
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
@@ -10,6 +11,7 @@ import Cookies from "js-cookie";
 
 // Images
 import ProfilePhoto from "../../assets/img/ProfilePhoto.png";
+import BackgroundSaldo from "../../assets/img/Background-Saldo.png"; // Tambahkan ini
 
 // Icons
 import { FaEye, FaEyeSlash } from "react-icons/fa";
@@ -17,7 +19,9 @@ import { FaEye, FaEyeSlash } from "react-icons/fa";
 export const GreetingCard = () => {
   const dispatch = useDispatch();
   const { profile } = useSelector((state) => state.profile);
-  const { balance, loading: balanceLoading } = useSelector((state) => state.transaction);
+  const { balance, loading: balanceLoading } = useSelector(
+    (state) => state.transaction
+  );
   const [showBalance, setShowBalance] = useState(false);
 
   useEffect(() => {
@@ -36,17 +40,21 @@ export const GreetingCard = () => {
     <div className="flex w-full flex-col gap-4 md:flex-row md:gap-0">
       <div className="w-full md:w-[40%]">
         <div className="flex flex-col items-center justify-center md:items-start md:justify-start">
-        <img
-          src={profile?.profile_image || ProfilePhoto}
-          alt="Foto Profil"
-          loading="lazy"
-          className="h-20 w-20 rounded-full border object-cover aspect-square md:h-16 md:w-16"
-        />
+          <img
+            src={profile?.profile_image || ProfilePhoto}
+            alt="Foto Profil"
+            loading="lazy"
+            className="h-20 w-20 rounded-full border object-cover aspect-square md:h-16 md:w-16"
+          />
 
           <div className="text-center md:text-left">
-            <p className="text-base text-slate-500 mt-2 md:mt-4">Selamat datang,</p>
+            <p className="text-base text-slate-500 mt-2 md:mt-4">
+              Selamat datang,
+            </p>
             <h1 className="text-2xl font-bold text-black">
-              {profile ? `${profile?.first_name} ${profile?.last_name}` : "Pengguna"}
+              {profile
+                ? `${profile?.first_name} ${profile?.last_name}`
+                : "Pengguna"}
             </h1>
           </div>
         </div>
@@ -54,18 +62,31 @@ export const GreetingCard = () => {
       <div className="w-full md:w-[60%]">
         <div
           className="rounded-md overflow-hidden p-5 flex flex-col gap-2 bg-cover bg-center bg-no-repeat"
-          style={{ backgroundImage: "url('/src/assets/img/Background-Saldo.png')" }}
+          style={{ backgroundImage: `url(${BackgroundSaldo})` }} // Gunakan variabel gambar
         >
           <p className="text-base text-white">Saldo Anda</p>
           <h1 className="text-3xl font-bold text-white flex items-center gap-2">
             Rp
             <span className="tracking-[5px]">
-              {balanceLoading ? "Loading..." : showBalance ? balance?.toLocaleString() : "**********"}
+              {balanceLoading
+                ? "Loading..."
+                : showBalance
+                ? balance?.toLocaleString()
+                : "**********"}
             </span>
           </h1>
-          <button onClick={toggleBalanceVisibility} className="flex items-center gap-2 text-sm text-white">
-            <span>{showBalance ? "Sembunyikan" : "Tampilkan"} Saldo</span>
-            {showBalance ? <FaEyeSlash size={15} className="text-white" /> : <FaEye size={15} className="text-white" />}
+          <button
+            onClick={toggleBalanceVisibility}
+            className="flex items-center gap-2 text-sm text-white"
+          >
+            <span>
+              {showBalance ? "Sembunyikan" : "Tampilkaan"} Saldo
+            </span>
+            {showBalance ? (
+              <FaEyeSlash size={15} className="text-white" />
+            ) : (
+              <FaEye size={15} className="text-white" />
+            )}
           </button>
         </div>
       </div>
